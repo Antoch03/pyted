f379817acc017a146580e27d185f87eeaa24940c
Anthony Rey <anthonyrey.simonnot@gmail.com>
2019-12-14 14:46:25 +0100

    update test

--- tests/test_pyted.py
+++ tests/test_pyted.py
@@ -1,11 +1,21 @@
+
 import unittest
 
 import pyted
 
 class TestMethods(unittest.TestCase):
+    def setUp(self):
+        pass 
+    
     def test_add(self):
         self.assertEqual(pyted.smile(), ":)")
-        def test_add_second(self):
-            self.assertEqual(pyted.frown(), ":(")
+    def test_add_second(self):
+        self.assertEqual(pyted.frown(), ":(")
+    def test_version(self):
+        self.assertEqual(pyted.__version__, "")
+    
+    def tearDown(self):
+        pass
+
 if __name__ == '__main__':
     unittest.main()
